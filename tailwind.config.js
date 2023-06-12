d1f13255361d3128894f1c302a13112cf044f87a
SirPhoros <cgtorrubia@gmail.com>
2023-06-12 09:49:43 +0100

    Tailwind Integrated

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	content: [],
+	content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
