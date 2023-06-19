0b5abc08b6c241019a4f129363d8d35218820b9c
alessandro-001 <alessandrofrondini@gmail.com>
2023-06-19 12:03:49 +0100

    added scroll feature in profile

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Button, Text, View, Image, StyleSheet } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { UserContext } from '../../Context/UserContext'
@@ -6,17 +6,30 @@ import { checkUser, getUserData } from '../../utils'
 import { TouchableOpacity } from 'react-native'
 import Egg from '../../assets/Egg.png'
 import Nest from '../../assets/Nest.png'
+import { ActivityIndicator } from 'react-native'
 
 function Home() {
 	const nav = useNavigation()
 	const { profileId, setProfileId } = useContext(UserContext)
+	const [loading, setLoading] = useState(true);
+
 
 	//Research about useEffect and why it is not reRendering
 	useEffect(() => {
 		getUserData().then((userData: any) => {
 			setProfileId(userData)
+			setLoading(false);
 		})
 	}, [])
+
+	/* Loading State */
+	if (loading) {
+		return (
+		  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<ActivityIndicator size="large" color="#D8A7B1" />
+		  </View>
+		);
+	  }
   
 	return (
 
