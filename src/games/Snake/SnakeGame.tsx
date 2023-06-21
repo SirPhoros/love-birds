b3861874680d410556c0e06be7cb363dd5adfdd6
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-06-21 10:53:51 +0100

    add ternary option for different game elements

--- src/games/Snake/SnakeGame.tsx
+++ src/games/Snake/SnakeGame.tsx
@@ -1,24 +1,56 @@
 import Game from "./components/Game"
 import "react-native-gesture-handler"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
-import { Text } from "react-native"
+import { Text, View } from "react-native"
 import { useRoute } from '@react-navigation/native';
+import Quiz from "../Quiz/Quiz"
 
 
+    function Snake () {
 
+        const route = useRoute()
+        const item = route.params?.item
+
+        console.log('gameName', item.game.gameName)
+
+        return (
+            <>
+                <GestureHandlerRootView style={{flex: 1}}>
+                <Game item={item}/>
+                </GestureHandlerRootView>
+            </>
+        )
+    }
+
+    function QuizGame () {
+
+        const route = useRoute()
+        const item = route.params?.item
+
+        console.log('gameName', item.game.gameName)
+
+        return (
+            <>
+                <GestureHandlerRootView style={{flex: 1}}>
+                <Quiz />
+                </GestureHandlerRootView>
+            </>
+        )
+    }
     
 
 const SnakeGame = () => {
     
     const route = useRoute()
     const item = route.params?.item
+
+    console.log('gameName', item.game.gameName)
     
 
     return (
     <>
-    <GestureHandlerRootView style={{flex: 1}}>
-    <Game item={item}/>
-    </GestureHandlerRootView>
+    {item.game.gameName === 'Snake' ? <Snake /> : <QuizGame />}
+    
     </>
 )
 }
